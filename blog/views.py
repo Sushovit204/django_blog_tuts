7bbaac8a48843689a7497a1bec36b6f26aca2f23
Sushovit204 <shahsushovit434@gmail.com>
2023-07-26 14:36:14 +0545

    Tweaked some css files and settings

--- blog/views.py
+++ blog/views.py
@@ -8,5 +8,5 @@ def posts(request):
     return render(request, "blog/all_posts.html")
 
 
-def post_details(request):
-    pass
+def post_details(request, slug):
+    return render(request, "blog/post-detail.html")
